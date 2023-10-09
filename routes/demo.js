f77acc60cbca146f002c24399d30c476f932cb92
Alin Triff <triffalin@gmail.com>
2023-10-09 14:59:53 +0300

    stored authentication data in sessions

--- routes/demo.js
+++ routes/demo.js
@@ -82,7 +82,11 @@ router.post('/login', async function (req, res) {
 		return res.redirect('login');
 	}
 
-	res.redirect('/admin');
+	req.session.user = { id: existingUser._id, email: existingUser.email };
+	req.session.isAuthenticated = true;
+	req.session.save(function () {
+		res.redirect('/admin');
+	});
 });
 
 router.get('/admin', function (req, res) {
