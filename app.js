6babf38fb12994cc265beec9080db31714fe3a9c
Alin Triff <triffalin@gmail.com>
2023-10-09 13:56:15 +0300

    added sessions support to our website

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const path = require('path');
 
 const express = require('express');
+const session = require('express-session');
+const mongodbStore = require('connect-mongodb-session');
 
 const db = require('./data/database');
 const demoRoutes = require('./routes/demo');
 
+const MongoDBStore = mongodbStore(session);
+
 const app = express();
+
+const sessionStore = new MongoDBStore({
+	uri: 'localhost:27017',
+	databaseName: 'auth-demo',
+	collection: 'sessions',
+});
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -13,12 +23,21 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
+app.use(
+	session({
+		secret: 'super-secret',
+		resave: false,
+		saveUninitialized: false,
+		store: sessionStore,
+	})
+);
+
 app.use(demoRoutes);
 
-app.use(function(error, req, res, next) {
-  res.render('500');
-})
+app.use(function (error, req, res, next) {
+	res.render('500');
+});
 
 db.connectToDatabase().then(function () {
-  app.listen(3000);
+	app.listen(3000);
 });
