3cfed8ce86db935700594464a67c835e69472592
Alin Triff <triffalin@gmail.com>
2023-10-09 12:32:35 +0300

    validated signup information

--- routes/demo.js
+++ routes/demo.js
@@ -23,6 +23,28 @@ router.post('/signup', async function (req, res) {
 	const enteredConfirmEmail = userData['confirm-email'];
 	const enteredPassword = userData.password;
 
+	if (
+		!enteredEmail ||
+		!enteredConfirmEmail ||
+		!enteredPassword ||
+		enteredPassword.trim() < 6 ||
+		enteredEmail !== enteredConfirmEmail ||
+		!enteredEmail.includes('@')
+	) {
+		console.log('Incorrect data');
+		return res.redirect('/signup');
+	}
+
+	const existingUser = await db
+		.getDb()
+		.collection('users')
+		.findOne({ email: enteredEmail });
+
+	if (existingUser) {
+		console.log('User exists already');
+		return res.redirect('/signup');
+	}
+
 	const hashedPassword = await bcrypt.hash(enteredPassword, 12);
 
 	const user = {
